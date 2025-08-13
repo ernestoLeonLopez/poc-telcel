4984902077a3c275bec9cc6f3374c1a86f37060e
Ernesto Leon <ernestoleon@wolfsellers.com>
2025-08-13 09:54:58 -0600

    quotes classes.

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,7 +1,14 @@
 export default function decorate(block) {
-  const [quoteWrapper] = block.children;
 
-  const blockquote = document.createElement('blockquote');
-  blockquote.textContent = quoteWrapper.textContent.trim();
-  quoteWrapper.replaceChildren(blockquote);
+     // Leer el valor de classes desde el dataset
+    const classes = block.dataset.classes || '';
+    if (classes) {
+        block.classList.add(...classes.split(' '));
+    }
+
+    const [quoteWrapper] = block.children;
+
+    const blockquote = document.createElement('blockquote');
+    blockquote.textContent = quoteWrapper.textContent.trim();
+    quoteWrapper.replaceChildren(blockquote);
 }
