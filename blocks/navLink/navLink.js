ff375fad206bff64bb2b6aeafd44c4881054c4be
ernestoLeonLopez <ernestoleon@wolfsellers.com>
2025-08-13 18:59:54 -0600

    Delete blocks/navLink directory

--- blocks/navLink/navLink.js
+++
@@ -1,8 +0,0 @@
-export default function decorate(block) {
-
-    const [navLinkWrapper] = block.children;
-
-    const blockquote = document.createElement('navLink');
-    blockquote.textContent = navLinkWrapper.textContent.trim();
-    navLinkWrapper.replaceChildren(blockquote);
-}
